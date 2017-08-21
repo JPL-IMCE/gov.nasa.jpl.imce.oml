f666c1286a3c62ef0bc0ca46ad133fe0a7950302
Devon Yates <devon.yates@jpl.nasa.gov>
2017-08-21 15:17:59 -0700

    Auto-gen code for DiagramUtilService

--- plugins/core/gov.nasa.jpl.imce.oml.viewpoint/xtend-gen/gov/nasa/jpl/imce/oml/viewpoint/DiagramUtil.java
+++ plugins/core/gov.nasa.jpl.imce.oml.viewpoint/xtend-gen/gov/nasa/jpl/imce/oml/viewpoint/DiagramUtil.java
@@ -2,24 +2,16 @@ package gov.nasa.jpl.imce.oml.viewpoint;
 
 import com.google.common.base.Objects;
 import com.google.common.collect.Iterables;
-import com.google.inject.Inject;
-import gov.nasa.jpl.imce.oml.dsl.scoping.OMLScopeExtensions;
 import gov.nasa.jpl.imce.oml.model.terminologies.Concept;
 import gov.nasa.jpl.imce.oml.model.terminologies.Entity;
 import gov.nasa.jpl.imce.oml.model.terminologies.EntityScalarDataProperty;
 import gov.nasa.jpl.imce.oml.model.terminologies.EntityStructuredDataProperty;
-import gov.nasa.jpl.imce.oml.model.terminologies.Term;
 import java.util.Set;
-import org.eclipse.xtext.xbase.lib.Extension;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 
 @SuppressWarnings("all")
 public class DiagramUtil {
-  @Inject
-  @Extension
-  private OMLScopeExtensions _oMLScopeExtensions;
-  
   /**
    * Returns a set of {@link EntityStructuredDataProperty}s that have the passed
    * {@link Concept} as its domain
@@ -29,10 +21,10 @@ public class DiagramUtil {
    */
   public Set<EntityStructuredDataProperty> getContainedEntityStructuredDataProperties(final Entity c) {
     final Function1<EntityStructuredDataProperty, Boolean> _function = (EntityStructuredDataProperty f) -> {
-      Term _relationDomain = f.relationDomain();
-      return Boolean.valueOf(Objects.equal(_relationDomain, c));
+      Entity _domain = f.getDomain();
+      return Boolean.valueOf(Objects.equal(_domain, c));
     };
-    return IterableExtensions.<EntityStructuredDataProperty>toSet(IterableExtensions.<EntityStructuredDataProperty>filter(Iterables.<EntityStructuredDataProperty>filter(this._oMLScopeExtensions.allEntityStructuredDataPropertiesScope(c.getTbox()).getAllElements(), EntityStructuredDataProperty.class), _function));
+    return IterableExtensions.<EntityStructuredDataProperty>toSet(IterableExtensions.<EntityStructuredDataProperty>filter(Iterables.<EntityStructuredDataProperty>filter(c.getTbox().getBoxStatements(), EntityStructuredDataProperty.class), _function));
   }
   
   /**
@@ -44,8 +36,8 @@ public class DiagramUtil {
    */
   public Set<EntityScalarDataProperty> getContainedEntityScalarDataProperties(final Entity c) {
     final Function1<EntityScalarDataProperty, Boolean> _function = (EntityScalarDataProperty f) -> {
-      Term _relationDomain = f.relationDomain();
-      return Boolean.valueOf(Objects.equal(_relationDomain, c));
+      Entity _domain = f.getDomain();
+      return Boolean.valueOf(Objects.equal(_domain, c));
     };
     return IterableExtensions.<EntityScalarDataProperty>toSet(IterableExtensions.<EntityScalarDataProperty>filter(Iterables.<EntityScalarDataProperty>filter(c.getTbox().getBoxStatements(), EntityScalarDataProperty.class), _function));
   }
