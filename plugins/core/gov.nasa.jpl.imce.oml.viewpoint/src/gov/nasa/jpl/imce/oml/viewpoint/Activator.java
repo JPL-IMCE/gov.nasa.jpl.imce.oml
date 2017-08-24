7081e696faa6e724df62494f49e2b518c744453b
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-08-24 10:10:04 -0700

    Added Sirius-related features for the OML UI Viewpoints

--- plugins/core/gov.nasa.jpl.imce.oml.viewpoint/src/gov/nasa/jpl/imce/oml/viewpoint/Activator.java
+++ plugins/core/gov.nasa.jpl.imce.oml.viewpoint/src/gov/nasa/jpl/imce/oml/viewpoint/Activator.java
@@ -35,7 +35,7 @@ public class Activator extends AbstractUIPlugin {
       super.start(context);
 	  plugin = this;
 	  viewpoints = new HashSet<Viewpoint>();
-	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/viewpoint.odesign")); 
+	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/OML.odesign")); 
     }
 
     /*
