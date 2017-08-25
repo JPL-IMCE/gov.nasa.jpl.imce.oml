6970b568d9d820085325cb5092419f705956d3b8
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-08-25 09:36:54 -0700

    incorrect logic for tagged vs. untagged builds

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -7,13 +7,6 @@ t=$(git name-rev --tags --name-only $(git rev-parse HEAD));
 
 if test "undefined" != "$t"; then
 
-	echo "#";
-	echo "# This is an untagged build.";
-	echo "#";
-	mvn -e clean verify;
-	
-else
-
     cat > ~/.m2/settings.xml << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
@@ -34,5 +27,12 @@ EOF
 	echo "# This is a tagged build: $t (if successful, artifacts will be deployed)";
 	echo "#";
     mvn -e deploy;
+
+else
+
+	echo "#";
+	echo "# This is an untagged build.";
+	echo "#";
+	mvn -e clean verify;
 
 fi
