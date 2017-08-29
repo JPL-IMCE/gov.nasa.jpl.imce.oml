9f93fe6c6694a36050cf99c0c2dfd8db9caa2331
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-08-29 16:01:31 -0700

    More tycho output filtering to avoid the 4Mb output limit

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -3,9 +3,11 @@
 awk '
 BEGIN {flag=0}
 /\[INFO\]\ Fetching\ /{next}
+/\[INFO\]\ Resolving\ /{next}
 /\[apply\]\ (\*|\{|\})/{next}
 /\[apply\]\ > (Host|Authorization|User-Agent|Accept|Content-Length|Expect):/{next}
 /\[apply\]\ < (Server|Date|Content-Type|Transfer-Enc.*|Connection):/{next}
 /(Down|Up)loading:/{flag+=1;next}
-/(Down|Up)loaded:/{flag-=1;if(flag>0)print}
+/Downloaded:/{flag-=1;next}
+/Uploaded:/{flag-=1;if(flag>0)print}
 {if(flag==0) print; else next;}'
