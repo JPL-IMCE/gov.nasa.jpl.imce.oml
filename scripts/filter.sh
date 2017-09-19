ca5c96c86483c270d362ac6196fe909bddec54af
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-09-19 13:39:00 -0700

    Filter out Progress at the end

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -3,8 +3,8 @@
 awk '
 BEGIN {flag=1}
 /\[INFO\]\ Reactor\ Build\ Order:/{flag=0;print;next}
-/Progress\ /{next}
 /(Down|Up)loading:/{flag+=1;next}
 /Downloaded:/{flag-=1;next}
 /Uploaded:/{flag-=1;if(flag>0)print}
+/Progress\ /{next}
 {if(flag==0) print; else next;}'
