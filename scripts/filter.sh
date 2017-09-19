25fbbea367befc0d2eec4581449bb39aa8a5269c
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-09-18 20:48:16 -0700

    Simplified filter again

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -4,7 +4,6 @@ awk '
 BEGIN {flag=0}
 /\[INFO\]\ Fetching\ /{next}
 /\[INFO\]\ Resolving\ /{next}
-/\[apply\]\ (\*|\{|\})/{next}
 /\[apply\]\ > (Host|Authorization|User-Agent|Accept|Content-Length|Expect):/{next}
 /\[apply\]\ < (Server|Date|Content-Type|Transfer-Enc.*|Connection):/{next}
 /(Down|Up)loading:/{flag+=1;next}
