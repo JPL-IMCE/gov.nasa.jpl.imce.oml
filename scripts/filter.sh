cd95fd3b747996f5df3909a772efa06c8f1a2f2e
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-09-20 11:07:35 -0700

    Restore some output to avoid travis job timeout

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 awk '
-BEGIN {flag=1}
-/\[INFO\]\ Reactor\ Build\ Order:/{flag=0;print;next}
+BEGIN {flag=0}
 /(Down|Up)loading:/{flag+=1;next}
 /Downloaded:/{flag-=1;next}
 /Uploaded:/{flag-=1;if(flag>0)print}
