8fbd8d00afb62d0d022844c1d9880ed2e1dc1f3d
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-09-22 21:09:54 -0700

    Filter out progress lines: '[INFO] Fetching ... (... at ...)'

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -4,5 +4,7 @@ awk '
 BEGIN {flag=0}
 /(Down|Up)loading:/{flag+=1;next}
 /Downloaded:/{flag-=1;next}
+/\[INFO\]\ Fetching\ [^[:blank:]]*\ from\ [^[:blank:]]* \([^[:blank:]]*\ at\ [^[:blank:]]*\)/{next}
+/\[INFO\]\ Fetching\ [^[:blank:]]*\ from\ [^[:blank:]]* \([^[:blank:]]*\ of\ [^[:blank:]]*\ at\ [^[:blank:]]*\)/{next}
 /Uploaded:/{flag-=1;if(flag>0)print}
 {if(flag==0) print; else next;}'
