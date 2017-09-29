30cfceaaf1239f007600112062dca4f22363a8a6
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-09-29 14:42:04 -0700

    awk filter

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -2,8 +2,9 @@
 
 awk '
 BEGIN {flag=0}
-/(Down|Up)loading:/{flag+=1;next}
-/Downloaded:/{flag-=1;next}
-/Uploaded:/{flag-=1;if(flag>0)print}
+/\[ERROR\].*/{print}
+/(Down|Up)loading:.*/{flag+=1;next}
+/Downloaded:.*/{flag-=1;next}
+/Uploaded:.*/{flag-=1;if(flag>0)print}
 /\[INFO\]\ Fetching\ [^ ]*\ from\ [^ ]*\ \(.*\ at\ [^ ]*\)/{next}
 {if(flag==0) print; else next;}'
