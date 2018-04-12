81e7e264dc28567d7a5c0882f7a926931e2496e9
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2018-04-12 13:20:19 -0700

    Fixes #280 (generated)

--- plugins/core/gov.nasa.jpl.imce.oml.model/xtend-gen/gov/nasa/jpl/imce/oml/model/extensions/OMLCatalogManager.java
+++ plugins/core/gov.nasa.jpl.imce.oml.model/xtend-gen/gov/nasa/jpl/imce/oml/model/extensions/OMLCatalogManager.java
@@ -3,20 +3,14 @@ package gov.nasa.jpl.imce.oml.model.extensions;
 import gov.nasa.jpl.imce.oml.model.extensions.OMLCatalog;
 import org.apache.xml.resolver.Catalog;
 import org.apache.xml.resolver.CatalogManager;
-import org.eclipse.xtext.xbase.lib.Exceptions;
 
 @SuppressWarnings("all")
 public class OMLCatalogManager extends CatalogManager {
   @Override
   public Catalog getPrivateCatalog() {
-    try {
-      final OMLCatalog catalog = new OMLCatalog();
-      catalog.setCatalogManager(this);
-      catalog.setupReaders();
-      catalog.loadSystemCatalogs();
-      return catalog;
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
-    }
+    final OMLCatalog catalog = new OMLCatalog();
+    catalog.setCatalogManager(this);
+    catalog.setupReaders();
+    return catalog;
   }
 }
